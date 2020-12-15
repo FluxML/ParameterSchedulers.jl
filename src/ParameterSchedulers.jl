f982349e72e56fdc7f07de0ff0bfaf8e8341a514
Kyle Daruwalla <daruwalla@wisc.edu>
2020-12-15 12:52:28 -0600

    Added simple step decay

--- src/ParameterSchedulers.jl
+++ src/ParameterSchedulers.jl
@@ -1,5 +1,12 @@
 module ParameterSchedulers
 
-# Write your package code here.
+using Animations
+using Base: @kwdef
+
+abstract type AbstractSchedule end
+
+export Step
+
+include("decay.jl")
 
 end
