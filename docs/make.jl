d8d690d5920a75d3df928286830aecaaccc30a6b
Kyle Daruwalla <daruwalla@wisc.edu>
2020-12-30 11:10:17 -0600

    Updated to force overwriting doc version folder

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,8 @@ using ParameterSchedulers
 p = Publish.Project(ParameterSchedulers)
 
 # needed to prevent error when overwriting
-# rm("dev", recursive = true, force = true)
-# rm(p.env["version"], recursive = true, force = true)
+rm("dev", recursive = true, force = true)
+rm(p.env["version"], recursive = true, force = true)
 
 # build documentation
-deploy(ParameterSchedulers; root = "/ParameterSchedulers.jl", label = "dev")
+deploy(ParameterSchedulers; root = "/ParameterSchedulers.jl", force = true, label = "dev")
