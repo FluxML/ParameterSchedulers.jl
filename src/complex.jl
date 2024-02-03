778d6c5c49dc1b78c08c8ebb263551392593cfb4
Kyle Daruwalla <daruwalla@wisc.edu>
2024-02-02 20:25:05 -0500

    Fix typo in OneCycle constructor

--- src/complex.jl
+++ src/complex.jl
@@ -240,8 +240,8 @@ up to `maxval` for `ceil(percent_start * nsteps)`, then back to `endval`
 (see [Super-Convergence: Very Fast Training of Neural Networks Using Large Learning Rates](https://arxiv.org/abs/1708.07120)).
 """
 function OneCycle(nsteps, maxval;
-                  startval = max / 25,
-                  endval = max / 1f5,
+                  startval = maxval / 25,
+                  endval = maxval / 1f5,
                   percent_start = 0.25)
     @assert 0 < percent_start < 1
 
