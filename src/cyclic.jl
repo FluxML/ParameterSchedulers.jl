a445060a4069edd6c4da19dcd70f0d4087dc5fc1
David Pätzel <david.paetzel@posteo.de>
2024-11-08 14:51:37 +0100

    Fix docstring of `CosAnneal`
    Fixes #67.

--- src/cyclic.jl
+++ src/cyclic.jl
@@ -186,7 +186,7 @@ A cosine annealing schedule
 (see ["SGDR: Stochastic Gradient Descent with Warm Restarts"](https://arxiv.org/abs/1608.03983v5))
 The output conforms to
 ```text
-t̂ = restart ? (t - 1) : mod(t - 1, period)
+t̂ = restart ? mod(t - 1, period) : (t - 1)
 abs(l0 - l1) * (1 + cos(π * t̂ / period)) / 2 + min(l0, l1)
 ```
 This schedule is also referred to as "cosine annealing (with warm restarts)"
